482ff335050fefa4048ab72d5e25b99cc02c04b5
Alexy Carlier <alexy.carlier@gmail.com>
2024-10-10 10:57:38 +0200

    Fix selected text during typing

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -7,10 +7,10 @@
             :ref="`ww-input-verification-code-${index}`"
             v-bind="content.input"
             @keydown.backspace="backspace(index)"
-            :ww-props="{ value: localValue[index] || ' '}"
+            :ww-props="{ value: localValue[index] || ' ' }"
             @paste="onPaste(index, $event)"
             @element-event="onChange(index, $event)"
-            @click="onFocused(index)"
+            @click="selectInput(index)"
         />
     </div>
 </template>
@@ -62,7 +62,10 @@ export default {
     },
     methods: {
         getInputComponentRef(index) {
-            return this.$refs[`ww-input-verification-code-${index}`]?.componentRef || this.$refs[`ww-input-verification-code-${index}`]?.[0]?.componentRef;
+            return (
+                this.$refs[`ww-input-verification-code-${index}`]?.componentRef ||
+                this.$refs[`ww-input-verification-code-${index}`]?.[0]?.componentRef
+            );
         },
         replaceAt(str, index, replacement) {
             return str.substring(0, index) + replacement + str.substring(index + replacement.length);
@@ -75,7 +78,7 @@ export default {
                 wwLib.wwLog.warn('WARNING [INPUT-CODE], failed to focus input');
             }
         },
-        onFocused(index) {
+        selectInput(index) {
             let input = this.getInputComponentRef(index);
             try {
                 input.selectInput();
@@ -92,27 +95,33 @@ export default {
             if (type !== 'update:value') return;
             value = value === null || value === undefined ? '' : `${value}`;
             value = value.trim();
+            let newFocusIndex = index;
             if (!value.length) {
                 value = ' ';
             } else {
-                const newFocusIndex = Math.min(this.content.nbrOfCode - 1, index + value.length);
-                if (index !== newFocusIndex) {
-                    this.focusInput(newFocusIndex);
-                }
+                newFocusIndex = Math.min(this.content.nbrOfCode - 1, index + value.length);
             }
             let newValue = this.replaceAt(`${this.localValue}`, index, value).substring(0, this.content.nbrOfCode);
             newValue = newValue.trim();
             this.localValue = '';
 
-            this.$nextTick( () => {
+            this.$nextTick(() => {
                 this.localValue = newValue;
                 this.setValue(`${this.localValue}`);
                 this.$emit('trigger-event', { name: 'change', event: { value: `${this.localValue}` } });
-            })
+                setTimeout(() => {
+                    this.focusInput(newFocusIndex);
+                    this.selectInput(newFocusIndex);
+                }, 0);
+            });
         },
         backspace(index) {
             if (index && (this.localValue[index] === ' ' || this.localValue[index] === undefined))
-                this.$nextTick(() => this.focusInput(index - 1));
+                this.$nextTick(() => {
+                    if (index !== newFocusIndex && index < this.content.nbrOfCode) {
+                        this.focusInput(newFocusIndex);
+                    }
+                });
         },
     },
 };
